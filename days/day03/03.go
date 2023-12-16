eb733237ecda7edcd300cabcf07c751186be63e3
uzizek-castoola <uros.zizek@castoola.com>
2023-12-16 17:53:12 +0100

    solved day 3 part 1

--- days/day03/03.go
+++ days/day03/03.go
@@ -11,76 +11,8 @@ import (
 
 var sum int = 0
 
-// Function to parse all lines and print out all found numbers in each line.
-func ParseNumbers(engine [][]byte) {
-
-	for i, line := range engine {
-		numbersInLine := []string{}
-		numStr := ""
-		numStartIndex, numEndIndex := -1, -1
-
-		for j, char := range string(line) {
-			if unicode.IsDigit(char) {
-				if numStartIndex == -1 {
-					numStartIndex = j
-				}
-				numEndIndex = j
-				numStr += string(char)
-			} else if numStr != "" {
-				numbersInLine = append(numbersInLine, numStr)
-
-				addIt := false
-
-				// check left and right
-				if (numStartIndex > 0 && isSymbol(string(line[numStartIndex-1]))) || (numEndIndex < (len(line)-1) && isSymbol(string(line[numEndIndex+1]))) {
-					fmt.Printf("adding left/right: %v\n", numStr)
-					addIt = true
-				}
-
-				// check above
-				if i > 0 {
-					for k := numStartIndex; k < numEndIndex; k++ {
-						if isSymbol(string(engine[i-1][k])) {
-							fmt.Printf("adding up: %v\n", numStr)
-							addIt = true
-							break
-						}
-					}
-				}
-
-				// check under
-				if i < (len(engine) - 1) {
-					for k := numStartIndex; k < numEndIndex; k++ {
-						if len(engine[i+1]) > k {
-							if isSymbol(string(engine[i+1][k])) {
-								fmt.Printf("adding up: %v\n", numStr)
-								addIt = true
-								break
-							}
-						}
-					}
-				}
-
-				if addIt {
-					addToSum(numStr)
-
-				}
-				numStr, numStartIndex, numEndIndex = "", -1, -1
-			}
-		}
-
-		if numStr != "" {
-			numbersInLine = append(numbersInLine, numStr)
-		}
-		if i < 332 {
-			fmt.Printf("Line %d: Numbers in this line: [%s]\n", i+1, strings.Join(numbersInLine, ", "))
-		}
-	}
-
-	fmt.Println(sum)
-}
-
 func Solve(part int, filePath string) {
+
 	if part < 1 || part > 2 {
 		fmt.Printf("Invalid part number: %v\n", part)
 		os.Exit(1)
@@ -100,7 +32,86 @@ func Solve(part int, filePath string) {
 		engine = append(engine, []byte(line))
 	}
 
-	ParseNumbers(engine)
+	for i, line := range engine {
+		numStr := ""
+		numStartIndex, numEndIndex := -1, -1
+
+		for j, char := range string(line) + "." {
+			if unicode.IsDigit(char) {
+				if numStartIndex == -1 {
+					numStartIndex = j
+				}
+				numEndIndex = j
+				numStr += string(char)
+			} else if numStr != "" {
+
+				addIt := false
+
+				// check left and right
+				if (numStartIndex > 0 && isSymbol(string(line[numStartIndex-1]))) || (numEndIndex < (len(line)-1) && isSymbol(string(line[numEndIndex+1]))) {
+					addIt = true
+				}
+
+				// check up
+				if i > 0 {
+					for k := numStartIndex; k <= numEndIndex; k++ {
+						if isSymbol(string(engine[i-1][k])) {
+							addIt = true
+							break
+						}
+					}
+				}
+
+				// check bottom
+				if i < (len(engine) - 1) {
+					for k := numStartIndex; k <= numEndIndex; k++ {
+						if len(engine[i+1]) > k {
+							if isSymbol(string(engine[i+1][k])) {
+								addIt = true
+								break
+							}
+						}
+					}
+				}
+
+				// check up-left corner
+				if i > 0 && numStartIndex > 0 {
+					if isSymbol(string(engine[i-1][numStartIndex-1])) {
+						addIt = true
+					}
+				}
+
+				// check up-right corner
+				if i > 0 && numEndIndex < (len(engine[i-1])-2) {
+					if isSymbol(string(engine[i-1][numEndIndex+1])) {
+						addIt = true
+					}
+				}
+
+				// check bottom-left corner
+				if i < (len(engine)-2) && numStartIndex > 0 && len(engine[i+1]) > (numStartIndex-1) {
+					if isSymbol(string(engine[i+1][numStartIndex-1])) {
+						addIt = true
+					}
+				}
+
+				// check bottom-right corner
+				if i < (len(engine)-2) && (numEndIndex+1) < len(engine[i+1]) {
+					if isSymbol(string(engine[i+1][numEndIndex+1])) {
+						addIt = true
+					}
+				}
+
+				if addIt {
+					addToSum(numStr)
+
+				}
+				numStr, numStartIndex, numEndIndex = "", -1, -1
+			}
+		}
+	}
+
+	fmt.Println(sum)
 }
 
 func isSymbol(ch string) bool {
