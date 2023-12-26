a3a55f549460f8f5aed6e59556cc9a50f0cece8c
uzizek-castoola <uros.zizek@castoola.com>
2023-12-26 16:20:53 +0100

    solved day 04 part 01

--- days/day04/04.go
+++ days/day04/04.go
@@ -2,7 +2,9 @@ package day04
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strings"
 )
 
 func Solve(part int, filePath string) {
@@ -12,12 +14,47 @@ func Solve(part int, filePath string) {
 		os.Exit(1)
 	}
 
-	_, err := os.ReadFile(filePath)
+	content, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Work in progress..")
+	lines := strings.Split(string(content), "\n")
+	sum := 0
 
+	for _, line := range lines {
+
+		lineSum := 0
+
+		index := strings.Index(line, ":")
+		trimmedText := line[index+1:]
+		parts := strings.Split(trimmedText, "|")
+
+		if len(parts) == 2 {
+			winning, my := strings.Split(parts[0], " "), strings.Split(parts[1], " ")
+			for _, card := range my {
+				if card != " " && card != "" && checkWinningCard(winning, card) {
+					lineSum += 1
+				}
+			}
+		}
+
+		if lineSum > 0 {
+			linePow := int(math.Pow(2, float64((lineSum - 1))))
+			sum += linePow
+		}
+	}
+
+	fmt.Println(sum)
+
+}
+
+func checkWinningCard(list []string, card string) bool {
+	for _, item := range list {
+		if card == item {
+			return true
+		}
+	}
+	return false
 }
