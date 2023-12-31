1728538771df48f262a92cfc1ba509de28c06c8e
uzizek-castoola <uros.zizek@castoola.com>
2023-12-31 11:21:08 +0100

    solved day 06 part 02

--- days/day06/day06.go
+++ days/day06/day06.go
@@ -24,27 +24,20 @@ func Solve(part int, filePath string) {
 	timeValues := parseValues(lines[0])
 	distanceValues := parseValues(lines[1])
 
-	if len(timeValues) != len(distanceValues) {
-		fmt.Println("Invalid input format.")
-		os.Exit(1)
+	if part == 2 {
+		timeValues = concatenateArray(timeValues)
+		distanceValues = concatenateArray(distanceValues)
+
 	}
 
-	switch part {
-	case 1:
-		result := calculateWaysToBeatRecord(timeValues, distanceValues)
-		fmt.Println(result)
-	case 2:
-		// Additional logic for part 2
-	default:
-		fmt.Printf("Invalid part number: %v\n", part)
-		os.Exit(1)
-	}
+	result := calculateWaysToBeatRecord(timeValues, distanceValues)
+	fmt.Println(result)
 }
 
 func parseValues(input string) []int {
 	values := []int{}
 	parts := strings.Fields(input)
-	for _, part := range parts[1:] { // Skip the first element
+	for _, part := range parts[1:] {
 		value, err := strconv.Atoi(part)
 		if err != nil {
 			fmt.Println("Error parsing input:", err)
@@ -69,4 +62,13 @@ func calculateWaysToBeatRecord(timeValues []int, distanceValues []int) int {
 		totalWays *= ways
 	}
 	return totalWays
+}
+
+func concatenateArray(arr []int) []int {
+	strNums := make([]string, len(arr))
+	for i, num := range arr {
+		strNums[i] = strconv.Itoa(num)
+	}
+	result, _ := strconv.Atoi(strings.Join(strNums, ""))
+	return []int{result}
 }
